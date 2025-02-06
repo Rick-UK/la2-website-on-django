9f2a3ebeaf6e6038a42ab196dcc5fcf0e2225728
Rick-UK <rick.uk@no.email>
2025-02-06 05:12:08 +0300

    2025-02-04_Added_index_html+styles

--- la2/urls.py
+++ la2/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'la2'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('news/<slug:news_slug>/', views.news_detail_page, name='news_detail_page'),
 ]
